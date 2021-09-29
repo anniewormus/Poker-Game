74399b51958e7cc3c2bc084796abd15165459799
anniewormus <annie@wormus.com>
2021-09-29 19:41:31 -0400

    initial commit

---
+++ poker.py
@@ -0,0 +1,15 @@
+''' 
+    This program is a simple python script that emulates a poker game
+    Created by Annie Wormus 9/23/2021
+'''
+# Set up function that determines player name, how many other players
+''' 
+    Returns:
+
+'''
+
+def set_up():
+    print("+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-+")
+    print("||\tWELCOME TO POKER NIGHT\t||")
+
+set_up()
